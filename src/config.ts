9757574278a66592805411f690db97922232c622
longday <6600gm@gmail.com>
2025-11-29 20:20:33 +0700

    fix: update APP_URL to point to the correct messenger service

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 import path from "node:path";
 import type { WindowBounds } from "./state/window";
 
-export const APP_URL = "https://yandex.ru/chat";
+//export const APP_URL = "https://yandex.ru/chat";
+export const APP_URL = "https://messenger.360.yandex.ru";
 
 export const USER_AGENT =
   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36";
